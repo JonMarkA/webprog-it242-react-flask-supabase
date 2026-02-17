d277250b451552018d788d0c4d9f5774ae9d04bd
JonMarkA <jvagbulos2@student.apc.edu.ph>
2026-02-17 11:10:46 +0000

    last commit?

--- individual-task/frontend/src/App.js
+++ individual-task/frontend/src/App.js
@@ -1,23 +1,14 @@
-import logo from './logo.svg';
+// frontend/src/App.js
+import React from 'react';
+import Profile from './components/Profile';
+import Guestbook from './components/Guestbook';
 import './App.css';
 
 function App() {
   return (
     <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
+      <Profile />
+      <Guestbook />
     </div>
   );
 }
