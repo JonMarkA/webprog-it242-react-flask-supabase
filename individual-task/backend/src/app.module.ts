b3130d6e8c59f7a19a8840a7aae8f98c7f7fabde
JonMarkA <jvagbulos2@student.apc.edu.ph>
2026-02-17 12:15:15 +0000

    Add CORS middleware and fix imports

--- individual-task/backend/src/app.module.ts
+++ individual-task/backend/src/app.module.ts
@@ -1,7 +1,12 @@
-import { Module } from '@nestjs/common';
+import { Module, NestModule, MiddlewareConsumer } from '@nestjs/common';
+import { CorsMiddleware } from './cors.middleware';
 import { GuestbookModule } from './guestbook/guestbook.module';
 
 @Module({
   imports: [GuestbookModule],
 })
-export class AppModule {}
+export class AppModule implements NestModule {
+  configure(consumer: MiddlewareConsumer) {
+    consumer.apply(CorsMiddleware).forRoutes('*');
+  }
+}
---
+++ individual-task/backend/src/cors.middleware.ts
@@ -0,0 +1,17 @@
+import { Injectable, NestMiddleware } from '@nestjs/common';
+import { Request, Response, NextFunction } from 'express';
+
+@Injectable()
+export class CorsMiddleware implements NestMiddleware {
+  use(req: Request, res: Response, next: NextFunction) {
+    res.header('Access-Control-Allow-Origin', 'https://webprog-it242-react-flask-supabase.vercel.app');
+    res.header('Access-Control-Allow-Methods', 'GET, POST, PUT, DELETE, OPTIONS');
+    res.header('Access-Control-Allow-Headers', 'Content-Type, Authorization');
+    res.header('Access-Control-Allow-Credentials', 'true');
+    
+    if (req.method === 'OPTIONS') {
+      return res.sendStatus(204);
+    }
+    next();
+  }
+}
